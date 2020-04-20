72571e063a70cd9068cf3293362913c91d539ad7
Claire Murray <16566310+cmurray-astro@users.noreply.github.com>
2020-04-20 15:54:20 -0400

    updated header

--- mcg2018_cube/decompose_smc.py
+++ mcg2018_cube/decompose_smc.py
@@ -1,9 +1,4 @@
-# @Author: riener
-# @Date:   2019-04-02T17:42:46+02:00
-# @Filename: decompose--grs.py
-# @Last modified by:   riener
-# @Last modified time: 2019-04-08T10:33:48+02:00
-
+# Modified by Claire Murray based on decompose--grs.py by Manuel Riener
 
 import os, glob
 
